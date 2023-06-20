2a6fa0086b9dcc012524e617a40f96b3b215b9bc
dh <kell0g_g@naver.com>
2023-06-20 13:19:03 +0900

    230620

--- src/숫자의_합_11720/Solution.kt
+++ src/숫자의_합_11720/Solution.kt
@@ -8,17 +8,16 @@ import java.io.OutputStreamWriter
 fun main(args: Array<String>) {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-
-    val n = br.readLine().toInt()
+    // n 은 필요없음
+    val n = br.readLine()
     val numbers = br.readLine()
     var sum = 0
     for (number in numbers) {
         sum += number - '0'
     }
     bw.write(String.format("%d\n", sum))
-    bw.flush()
-    br.close()
     bw.close()
+    br.close()
 }
 
 class Solution {
